6d3cbfa4902a724de4c7c7441aa3cc3d20c22a14
S. Tiso <P288427@Workspace.rug.nl>
2020-03-03 18:30:15 +0100

    all individuals are initialized at a certain position with energy 0

--- individual.cpp
+++ individual.cpp
@@ -1,8 +1,10 @@
 #include "individual.h"
 #include <cassert>
 
-individual::individual(double size):
-    m_size(size)
+individual::individual(double x_pos, double y_pos, double size):
+    m_size(size),
+    m_x(x_pos),
+    m_y(y_pos)
 {
 
 }
@@ -10,8 +12,26 @@ individual::individual(double size):
 void test_individual(){
 
     //An individual should be initialized with the defined starting size
-    double starting_size = 14.0;
-    individual i(starting_size);
-    assert(i.get_size() - starting_size < 0.0000001);
+    {
+        double starting_size = 14.0;
+        individual i(0,0,starting_size);
+        assert(i.get_size() - starting_size < 0.0000001);
+    }
+
+    //Individuals should be initialized with 0 internal energy
+    {
+        individual i(0,0);
+        assert(i.get_energy() - 0.0 < 0.000001);
+    }
+
+    //An inddividual should be initialized at a certain position
+    {
+        double x = 100;
+        double y = 100;
+        individual i(x,y);
+        assert(i.get_x() - x < 0.0000001);
+        assert(i.get_y() - y < 0.0000001);
+    }
+
 
 }
--- individual.h
+++ individual.h
@@ -5,13 +5,27 @@
 class individual
 {
 public:
-    individual(double size);
 
-    ///gets size of individual
+    individual(double x_pos, double y_pos, double size  = 0);
+
+     ///gets size of individual
      double get_size() const noexcept {return m_size;}
 
+     ///gets energy of individual
+     double get_energy() const noexcept {return m_energy;}
+
+     ///gets x position of the individual
+     double get_x() const noexcept {return m_x;}
+
+     ///gets y position of the individual
+     double get_y() const noexcept {return m_y;}
+
 private:
+
     double m_size;
+    double m_energy = 0;
+    double m_x;
+    double m_y;
 };
 
 void test_individual();
