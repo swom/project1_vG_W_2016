cc6228becd21d00a30242dbfc0026131a9763f26
S. Tiso <P288427@Workspace.rug.nl>
2020-03-04 12:02:24 +0100

    Adding function to detect individuals ready to divide

--- simulation.cpp
+++ simulation.cpp
@@ -2,17 +2,27 @@
 #include <cassert>
 
 simulation::simulation(int pop_size):
-    m_vector_of_individuals(pop_size,individual(0,0))
+    population(pop_size,individual(0,0))
 {
 
+}
+
+std::vector<individual> simulation::get_dividing_individuals(){
+    std::vector<individual> dividing_individuals;
+
+    for(auto ind : population){
+        if(ind.get_energy()>= ind.get_treshold_energy()){dividing_individuals.push_back(ind);}
+    }
+    return dividing_individuals;
 }
 
 void simulation::individuals_divide(std::vector<individual> dividing_individuals){
 
     for(auto ind : dividing_individuals){
-        m_vector_of_individuals.push_back(ind);
+        population.push_back(ind);
     }
 }
+
 
 
 
@@ -23,7 +33,7 @@ void test_simulation()
         int pop_size = 100;
         simulation s(pop_size);
         // The value 1234567890 is irrelevant: just get this to compile
-        for(unsigned int i = 0; i < s.get_v_individuals().size(); ++i)
+        for(unsigned int i = 0; i < s.get_pop().size(); ++i)
         {
         assert( s.get_individual(i).get_x() > -1234567890 );
         }
@@ -32,17 +42,33 @@ void test_simulation()
     //The size of a population is equal to the size of the vector containing its individuals
     {
         simulation s;
-        assert( s.get_pop_size() == static_cast<int>(s.get_v_individuals().size()));
+        assert( s.get_pop_size() == static_cast<int>(s.get_pop().size()));
     }
 
     //when an individual reproduces it adds a copy of itself to the population/individual vector(i.e divides)
     {
         simulation s;
         double lhs = s.get_pop_size();
-        s.individuals_divide(s.get_v_individuals());
+        //let's allow all individuals in the population to reproduce, to facilitate the testing conditions
+        s.individuals_divide(s.get_pop());
         double rhs = s.get_pop_size();
         assert(lhs * 2 - rhs < 0.0000001);
 
     }
 
+    //Individuals that have more than the energy treshold value are put in the dividing individuals vector
+
+    //NOT SURE HOW TO BE MORE PRECISE, should add ID to individuals and create a == operator for individual class
+    //for now I will just check that the reproducing_individuals vector has the same size as the number of reproducing individuals
+
+    //No individuals are dividing at the start of the simulation
+    {
+      std::vector<individual> dividing_individuals;
+      std::vector<int> future_dividing_ind_indexes{0,1};
+      simulation s(3);
+
+      int lhs = dividing_individuals.size();
+      assert(static_cast<int>(s.get_dividing_individuals().size())  - lhs < 0.00000000001);
+
+    }
 }
--- simulation.h
+++ simulation.h
@@ -8,23 +8,26 @@ class simulation
 public:
     simulation(int pop_size = 1);
 
-    ///Gets the vector containing all the individuals of the population
-    const std::vector<individual>& get_v_individuals() const noexcept {return m_vector_of_individuals;}
+    ///finds the individuals ready to divide
+    std::vector<individual> get_dividing_individuals();
 
     ///Gets an inidividual at a certain index in the vector
-    const individual& get_individual(int i) const noexcept {return m_vector_of_individuals[i];}
+    const individual& get_individual(int i) const noexcept {return population[i];}
 
     ///Gets an inidividual at a certain index in the vector and allows to change it
-    individual& get_individual(int i) noexcept {return m_vector_of_individuals[i];}
+    individual& get_individual(int i) noexcept {return population[i];}
 
     ///Gets the size of the population
-    int get_pop_size() const noexcept {return static_cast<int>(m_vector_of_individuals.size());}
+    int get_pop_size() const noexcept {return static_cast<int>(population.size());}
+
+    ///Gets the vector containing all the individuals of the population
+    const std::vector<individual>& get_pop() const noexcept {return population;}
 
     ///The individuals in the vector are copied at the end of the pop vector
     void individuals_divide(std::vector<individual> dividing_individuals);
 
 private:
-    std::vector<individual> m_vector_of_individuals;
+    std::vector<individual> population;
 };
 
 void test_simulation();
