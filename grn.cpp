20f05927ab0417e0cd9d6cba262c1e8e7e9ba153
swom <cicciacco@gmail.com>
2020-03-24 12:40:09 +0100

    working on #28, broke down into smaller functions big messy update function. Now there are two functions, input_updates_hidden and hidden_updates_ouput

--- grn.cpp
+++ grn.cpp
@@ -15,11 +15,68 @@ GRN::GRN(size_t n_input, size_t n_hidden, size_t n_output):
 
 }
 
+void GRN::set_hid_node(int index_node, bool state)
+{m_ExHidden[static_cast<unsigned int>(index_node)] = state;}
+
+void GRN::set_H2O(double value) noexcept
+{
+  for (auto& node : get_H2O())
+    {
+      for(auto& weight : node)
+      weight = value;
+    }
+}
+
+void GRN::set_I2H(bool value) noexcept
+{
+  for (auto& node : get_I2H())
+    {
+      for(size_t i = 0; i != node.size(); i++)
+      node[i] = value;
+    }
+}
+
+void GRN::set_all_out_nodes(bool state)
+{
+  for(int i = 0; i != static_cast<int>(get_output_nodes().size()); i++)
+    set_out_node(i, state);
+}
+
+
+void GRN::set_all_hid_nodes(bool state)
+{
+  for(int i = 0; i != static_cast<int>(get_hidden_nodes().size()); i++)
+    set_hid_node(i,state);
+}
+
+void GRN::set_all_hid_tresh(double value)
+{
+  for(auto& treshold : get_hid_tresh())
+     treshold = value;
+}
+
+void GRN::set_all_out_tresh(double value)
+{
+  for(auto& treshold : get_out_tresh())
+     treshold = value;
+}
+
 void GRN::set_out_node(int index_node, bool state)
 {m_ExOutput[static_cast<unsigned int>(index_node)] = state;}
 
-void GRN::set_hid_node(int index_node, bool state)
-{m_ExHidden[static_cast<unsigned int>(index_node)] = state;}
+void hid_updates_out(GRN& g) noexcept
+{
+  for(unsigned int i = 0; i != g.get_output_nodes().size(); i++)
+    {
+      double signal_out = 0;
+      for (unsigned int j = 0 ; j != g.get_hidden_nodes().size(); j++)
+        {
+          signal_out  += g.get_hidden_nodes()[j] * g.get_H2O()[j][i];
+
+        }
+      if(signal_out > g.get_out_tresh()[i]){g.set_out_node(static_cast<int>(i),signal_out);}
+    }
+}
 
 void jordi_response(GRN& g, std::vector<double> inputs) //Jordi style
 {
@@ -27,19 +84,10 @@ void jordi_response(GRN& g, std::vector<double> inputs) //Jordi style
   //First he updates output from hidden (is not the first thing he does
   //in the code but is the first thing that actually changes something in the
   //GRN
-  for(unsigned int i = 0; i != g.get_output_states().size(); i++)
-    {
-      double signal_out = 0;
-      for (unsigned int j = 0 ; j != g.get_hidden_states().size(); j++)
-        {
-          signal_out  += g.get_hidden_states()[j] * g.get_H2O()[j][i];
-
-        }
-      if(signal_out > g.get_out_tresh()[i]){g.set_out_node(static_cast<int>(i),signal_out);}
-    }
+  hid_updates_out(g);
 
   //Update the state of the hidden nodes
-  for(unsigned int i = 0; i != g.get_hidden_states().size(); i++)
+  for(unsigned int i = 0; i != g.get_hidden_nodes().size(); i++)
     {
       double signal_hid = 0;
       for (unsigned int j = 0 ; j != g.get_input_states().size(); j++)
@@ -93,18 +141,88 @@ void test_GRN()//!OClint, tests may be long
     GRN g;
     for(auto input : g.get_input_states())
       assert(input < 0.00001 && input > -0.00001);
-    for(auto hidden : g.get_hidden_states())
+    for(auto hidden : g.get_hidden_nodes())
       assert(hidden < 0.00001 && hidden > -0.00001);
-    for(auto output : g.get_output_states())
+    for(auto output : g.get_output_nodes())
       assert(output < 0.00001 && output > -0.00001);
   }
 
+  //All weights in H2O connection can be set to a value
+  {
+    GRN g;
+    double weight_value = 3.14;
+    g.set_H2O(weight_value);
+    for(const auto& node : g.get_H2O())
+      for(const auto& weight : node)
+        assert(weight - weight_value < 0.00001 &&
+               weight - weight_value > -0.00001);
+  }
+
+  //All output nodes can be set to true or false
+  {
+    GRN g;
+    bool nodes_state = false;
+    g.set_all_out_nodes(nodes_state);
+    for(auto node : g.get_output_nodes())
+      assert(node == nodes_state);
+  }
+  //All output nodes treshold can be set to a value
+  {
+    GRN g;
+    double treshold_value = 3.14;
+    g.set_all_out_tresh(treshold_value);
+    for (const auto& treshold : g.get_out_tresh())
+      {
+        assert(treshold - treshold_value < 0.00001 &&
+               treshold - treshold_value > -0.00001);
+      }
+  }
+  //All hidden nodes can be set to true or false
+  {
+    GRN g;
+    bool nodes_state = false;
+    g.set_all_hid_nodes(nodes_state);
+    for(auto node : g.get_hidden_nodes())
+      assert(node == nodes_state);
+  }
+
+  //Output nodes can be updated based on values of hidden nodes
+  {
+    GRN g;
+    bool hidden_nodes_value = 1;
+    //all hidden nodes are 1
+    g.set_all_hid_nodes(hidden_nodes_value);
+    //all weight H20 are
+    //hidden_nodes_value / number_of_connections / number_of_output_nodes
+    g.set_H2O(1 *
+              g.get_output_nodes().size() *
+              (g.get_H2O().size() * g.get_H2O()[0].size()));
+    //Therefore each output node will receive a signal of hidden_nodes_value
+    g.set_all_out_tresh(hidden_nodes_value - 0.00001);
+    hid_updates_out(g);
+    for (size_t i = 0; i != g.get_output_nodes().size(); i++)
+    assert(g.get_output_nodes()[i] == 1);
+  }
+
+  //Given the initial inputs the hidden nodes can be updated
+  {
+    GRN g;
+    double inputs_value = 3.14; //in this case all inputs are the same
+    std::vector<double> input(g.get_input_states().size(), inputs_value);
+    g.set_all_hid_tresh(inputs_value - 0.00001);
+    g.set_I2H(g.get_hidden_nodes().size() *
+             g.get_I2H().size() * g.get_I2H()[0].size());
+    inp_updates_hid(g);
+    for (size_t i = 0; i != g.get_hidden_nodes().size(); i++)
+    assert(g.get_hidden_nodes()[i] == 1);
+
+  }
   //Given three initial input a GRN gives back an output
   {
     GRN g;//the weights and connection and nodes are all to 0, so output should be 0
     std::vector<double> inputs{100,100,100};
     jordi_response(g, inputs);
-    for(auto output : g.get_output_states())
+    for(auto output : g.get_output_nodes())
       {
         assert(output < 0.00001
                && output > -0.00001);
--- grn.h
+++ grn.h
@@ -7,32 +7,53 @@ class GRN
 public:
   GRN(size_t n_input = 3, size_t n_inner = 3, size_t n_output = 2);
 
-  ///Gets the connection from input to hidden layer
+  ///Gets the constant reference to connection from input to hidden layer
   const std::vector<std::vector<double> >& get_I2H() const noexcept {return m_ConI2H;}
+
+  ///Gets the reference to connection from input to hidden layer
+  std::vector<std::vector<double> >& get_I2H() noexcept {return m_ConI2H;}
 
   ///Gets the connection from hidden to hidden layer
   const std::vector<std::vector<double> >& get_H2H() const noexcept {return m_ConH2H;}
 
-  ///Gets the connection from hidden to output layer
+  ///Gets the const ref to connection from hidden to output layer
   const std::vector<std::vector<double> >& get_H2O() const noexcept {return m_ConH2O;}
+
+  ///Gets the reference connection from hidden to output layer
+  std::vector<std::vector<double> >& get_H2O() noexcept {return m_ConH2O;}
 
   ///Gets const reference to input layer states
   const std::vector<double>& get_input_states() const noexcept {return  m_ExInput;}
 
   ///Gets const reference hidden layer states
-  const std::vector<bool>& get_hidden_states() const noexcept {return  m_ExHidden;}
+  const std::vector<bool>& get_hidden_nodes() const noexcept {return  m_ExHidden;}
+
+  ///Gets reference hidden layer states
+  std::vector<bool>& get_hidden_nodes() noexcept {return  m_ExHidden;}
 
   ///Gets const ref to tresholds of hidden nodes
   const std::vector<double>& get_hid_tresh() const noexcept {return m_THidden;}
 
+  ///Gets ref to tresholds of hidden nodes
+  std::vector<double>& get_hid_tresh() noexcept {return m_THidden;}
+
   ///Gets const reference output layer states
-  const std::vector<bool>& get_output_states() const noexcept {return  m_ExOutput;}
+  const std::vector<bool>& get_output_nodes() const noexcept {return  m_ExOutput;}
 
   ///Gets reference output layer states
-  std::vector<bool>& get_output_states() noexcept {return  m_ExOutput;}
+  std::vector<bool>& get_output_nodes() noexcept {return  m_ExOutput;}
 
   ///Gets const ref to tresholds of output nodes
   const std::vector<double>& get_out_tresh() const noexcept {return m_TOutput;}
+
+  ///Gets ref to tresholds of output nodes
+  std::vector<double>& get_out_tresh() noexcept {return m_TOutput;}
+
+  ///Sets all weights of H2O to a given value
+  void set_H2O(double value) noexcept;
+
+  ///Sets all weights of I2H to a given value
+  void set_I2H(bool value) noexcept;
 
   ///Sets an output node to true or false
   void set_hid_node(int index_node, bool state);
@@ -40,6 +61,17 @@ public:
   ///Sets an output node to true or false
   void set_out_node(int index_node, bool state);
 
+  ///Sets all output nodes to a state: T/F
+  void set_all_hid_nodes(bool state);
+
+  ///Sets all output nodes to a state: T/F
+  void set_all_out_nodes(bool state);
+
+  ///Sets all the tresholds of the output nodes to a certain value
+  void set_all_out_tresh(double value);
+
+  ///Sets all the tresholds of the hidden nodes to a certain value
+  void set_all_hid_tresh(double value);
 
 private:
   std::vector<std::vector<double> > m_ConI2H;	// Connections from Input to Hidden layer
@@ -53,6 +85,9 @@ private:
   std::vector<bool> m_ExHidden;			// Expression during 'development'
   std::vector<bool> m_ExOutput;			// Expression during 'development'
 };
+
+///Hidden nodes update the states of output nodes
+void hid_updates_out(GRN& g) noexcept;
 
 ///The GRN reads inputs and gives back the outputs
 ///!!!!****Implemented as in Jordi's model***!!! I do not like it
