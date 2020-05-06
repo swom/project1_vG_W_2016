7fe01da449dc8e6ad70edf3bc7875b95c87ad047
swom <cicciacco@gmail.com>
2020-05-06 14:25:11 +0200

    first commit

--- env_param.cpp
+++ env_param.cpp
@@ -1,4 +1,5 @@
 #include "env_param.h"
+#include <cassert>
 
 env_param::env_param(int grid_side,
                      double diff_coeff,
@@ -10,6 +11,12 @@ env_param::env_param(int grid_side,
     m_metab_degradation_rate{metab_degrad_rate}
 {
 
+    assert(m_diff_coeff > -0.000000000001 &&
+           m_diff_coeff < 1.000000000001);
+    assert(m_grid_side >= 0);
+    assert(m_init_food > -0.000000000001);
+    assert(m_metab_degradation_rate > -0.000000001 &&
+           m_metab_degradation_rate < 0.000000001);
 }
 
 void test_env_param() noexcept //!OCLINT
--- ind_param.cpp
+++ ind_param.cpp
@@ -1,4 +1,5 @@
 #include "ind_param.h"
+#include <cassert>
 
 ind_param::ind_param(double radius,
                      double treshold_energy,
@@ -15,7 +16,13 @@ ind_param::ind_param(double radius,
     m_uptake_rate{uptake_rate},
     m_wiggle_room{wiggle_room}
 {
-
+    assert(m_metabolic_rate > 0);
+    assert(m_metab_secr_rate > 0);
+    assert(m_radius > 0);
+    assert(m_transformation_time > 0);
+    assert(m_treshold_energy > 0);
+    assert(m_uptake_rate > 0);
+    assert(m_wiggle_room > 0 && m_wiggle_room < m_radius);
 }
 
 void test_ind_param() noexcept  //!OCLINT
