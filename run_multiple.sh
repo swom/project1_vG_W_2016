e1d3cc9454900c609e068264023633633553f584
swom <cicciacco@gmail.com>
2020-06-08 16:36:05 +0200

    changed .sh file to work with current program

--- run_multiple.sh
+++ run_multiple.sh
@@ -22,7 +22,11 @@
 # simulation_logic_only has this command-line interface:
 #
 # simulation_logic_only [seed] [output filename]
-
+module load Qt5
+module load gompic/2019b
+module load gompi
+qmake simulation_logic_only.pro
+make 
 for i in $(seq 1 2)
 do
   echo $i
