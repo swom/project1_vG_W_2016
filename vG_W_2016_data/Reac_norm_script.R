bb0694e58dd77a5e569362d4109a37ab27346661
S. Tiso <P288427@Workspace.rug.nl>
2020-09-16 18:06:53 +0200

    adding rscript for reaction norms

---
+++ vG_W_2016_data/Reac_norm_script.R
@@ -0,0 +1,9 @@
+library(tidyr)
+library(dplyr)
+library(stringr)
+library(ggplot2)
+reac_norm = data.frame()
+
+for (i in  list.files(path = '.',pattern = "reaction_norm_best_ind_s\\d+_f\\d+.csv"))
+{
+}
