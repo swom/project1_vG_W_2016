8c4b443021fe6527afc102edd41a910bf5412121
S. Tiso <P288427@Workspace.rug.nl>
2020-12-04 02:28:30 +0100

    olè

--- main_logic_only.cpp
+++ main_logic_only.cpp
@@ -28,8 +28,6 @@ void test() {
 int main(int argc, char ** argv) //!OCLINT tests may be long
 {
 
-    std::cout << "start" << std::endl;
-
     const std::vector<std::string> args(argv, argv + argc);
 
 #ifndef NDEBUG
