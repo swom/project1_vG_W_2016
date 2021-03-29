af7286151886a2056125e0ae810b29f8be0615db
S. Tiso <P288427@Workspace.rug.nl>
2021-03-29 16:02:29 +0200

    added function to run tests against first and last gen of rand_evo

--- simulation.cpp
+++ simulation.cpp
@@ -1082,7 +1082,7 @@ demographic_sim run_test_extreme_rand_evo_beginning_end(int original_seed,
 
     auto new_s = load_sim_no_pop(original_seed, original_change, prefix);
 
-    int n_rand_cond = 100;
+    int n_rand_cond = 10;
     int pop_max = 10000;
 
     auto save_name = prefix + create_test_random_condition_name(amplitude, original_change, original_seed);
