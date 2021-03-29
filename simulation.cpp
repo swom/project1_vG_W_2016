9046600b6b8d54fac2e2b3b8091bbfed78bd1ed2
S. Tiso <P288427@Workspace.rug.nl>
2021-03-29 18:36:42 +0200

    adding #define for when on linux so that it loads from data partition when running on cluster for test evo rand

--- simulation.cpp
+++ simulation.cpp
@@ -1084,7 +1084,7 @@ demographic_sim run_test_extreme_rand_evo_beginning_end(int original_seed,
 #ifdef ON_LINUX
     data_folder = "../../../data/p288427/";
 #endif
-    auto prefix = data_folder + create_rand_extreme_prefix(cond_per_seq, seq_index, amplitude);
+    auto prefix = data_folder + create_rand_extreme_prefix(seq_index, cond_per_seq, amplitude);
     std::cout << prefix << std::endl;
 
     //little trick added for now since sim_par are not saved with prefix
